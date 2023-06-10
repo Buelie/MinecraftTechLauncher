8e25c4cbcd4d1398dfbd6f6dddb0ea72a2c6e401
QingYunStudio <111875719+Buelie@users.noreply.github.com>
2023-06-10 13:12:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,56 @@
 from tkinter import MULTIPLE, ttk
 import tkinter as tk
 import json
+import os
+import webbrowser
+import sys
+from tkinter.scrolledtext import ScrolledText
+from tkinter.filedialog import *
+import tkinter.colorchooser
+import subprocess
+from tkinter import messagebox
 
 MainForm = tk.Tk()
 
+def SettingMain():
+    
+    class Stdout():	# 重定向类
+        def __init__(self):
+        	# 将其备份
+            self.stdoutbak = sys.stdout		
+            self.stderrbak = sys.stderr
+            # 重定向
+            sys.stdout = self
+            sys.stderr = self
+
+        def write(self, info):
+            # info信息即标准输出sys.stdout和sys.stderr接收到的输出信息
+            JavaPathL.insert('end', info)# 在多行文本控件最后一行插入print信息
+            JavaPathL.update()# 更新显示的文本，不加这句插入的信息无法显示
+            JavaPathL.see(tk.END)# 始终显示最后一行，不加这句，当文本溢出控件最后一行时，不会自动显示最后一行
+
+        def restoreStd(self):
+            # 恢复标准输出
+           sys.stdout = self.stdoutbak
+           sys.stderr = self.stderrbak
+
+    def jp():
+        #a = os.popen('where java')
+
+        b = subprocess.Popen('where java')
+        a = b.stdout.read()
+        b.wait()
+        b.stdout.close()
+        return a
+    
+    SetMain = tk.Toplevel()
+    SetMain.title('MinecraftTechLauncher')
+    SetMain.geometry("600x450+374+182") #设置窗口大小
+    SetMain.iconphoto(False,tk.PhotoImage(file='icon.png'))
+
+    JavaPathLable = ttk.Label(SetMain,text='Java路径:').grid(row=0,column=0)
+    JPath = os.system('where java')
+    JavaPathL = ttk.Label(SetMain,text=jp)
 def DownTovM(): #游戏本体下载子窗口
     DownFarm = tk.Toplevel()
     DownFarm.title('MinecraftTechLauncher') #设置标题
@@ -35,12 +82,37 @@ Json_Ver = ReadFile('config/c.json')
 VerVar = tk.StringVar() #存放版本列表
 VerVar.set((ReadFile("config/c.json")['ver']['Minecraft'])) #存放版本列表注册到列表
 
-VerMenu = tk.Listbox(MainForm,name='版本列表',listvariable=VerVar).grid(row=0,column=0) #存放版本列表注册到列表
-
-StartGameBtn = ttk.Button(MainForm,text='启动游戏').grid(row=1,column=0) #启动游戏按钮
+VerMenu = tk.Listbox(MainForm,name='版本列表',listvariable=VerVar) #存放版本列表注册到列表
 
 VerLen_L = ttk.Label(MainForm,text="游戏版本数量:").grid(row=0,column=1)
 VerLen = ttk.Label(MainForm,text=str(len(ReadFile("config/c.json")['ver']['Minecraft']))).grid(row=0,column=2)
+
+JavaPthLable = ttk.Label(MainForm,text=' | Java路径:').grid(row=1,column=3)
+JavaPth = ttk.Entry(MainForm)
+
+BigNC = ttk.Label(MainForm,text='最大内存:').grid(row=1,column=1)
+BiggestNC = ttk.Entry(MainForm)
+
+def startgame():
+    if JavaPth.get() == "":
+        messagebox.showerror("错误","请填写Java路径")
+    else:
+        if VerMenu.curselection() == ():
+            messagebox.showerror("错误","请选择版本")
+        elif BiggestNC.get() == "":
+            messagebox.showerror("错误","请填写最大内存")
+        else:
+            messagebox.showinfo("提示","启动成功")
+            try:
+                SatartCS = JavaPth.get() + ""
+            except Exception as e:
+                print(e)
+                return e
+
+BiggestNC.grid(row=1,column=2)
+VerMenu.grid(row=0,column=0)
+JavaPth.grid(row=1,column=4)
+StartGameBtn = ttk.Button(MainForm,text='启动游戏',command=startgame).grid(row=1,column=0) #启动游戏按钮
 ################# 启动器菜单栏 #################
 MainMenu = tk.Menu(MainForm,relief='solid')
 
@@ -48,7 +120,7 @@ StarterMenu_1 = tk.Menu(MainMenu,tearoff=False)
 StarterMenu_1.add_command(label="设置")
 StarterMenu_1.add_command(label="退出",command=MainForm.quit)
 StarterMenu_1.add_separator()
-StarterMenu_1.add_command(label="高级设置")
+StarterMenu_1.add_command(label="高级设置",command=SettingMain)
 MainMenu.add_cascade(label="启动器",menu=StarterMenu_1)
 
 StarterMenu_2 = tk.Menu(MainMenu,tearoff=False)
