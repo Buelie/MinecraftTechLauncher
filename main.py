e9d5e2d49771c67615248e66138430c1ec890a65
QingYunStudio <111875719+Buelie@users.noreply.github.com>
2023-06-23 16:18:24 +0800

    重构(#2)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
 import os
 import json
+import tkinter as tk
+import tkinter.ttk as ttk
 
 # 文件检测/初始化
 if os.path.isdir('.minecraft/') == False:
@@ -52,7 +54,7 @@ class game:
     def __init__(self,cfg : str = "") -> None:
         self.cfg = cfg
 
-    # TODO:待完成
+    # TODO:待完成MainClass的拼接
     def build(self,auth_player_name : str = "test",version_name : str = "1.0",game_directory : str = "",assets_root : str = "",assets_index_name : str = "1.0",auth_uuid : str = "12345678-012345678921-3456709312X456",auth_access_token : str = " ",user_properties : str = "{}",user_type : str = "legacy"):
         default = "--username ${auth_player_name} --version ${version_name} --gameDir ${game_directory} --assetsDir ${assets_root} --assetIndex ${assets_index_name} --uuid ${auth_uuid} --accessToken ${auth_access_token} --userProperties ${user_properties} --userType ${user_type}"
         parameter = default.replace("${auth_player_name}",auth_player_name).replace("${version_name}",version_name)\
@@ -61,4 +63,39 @@ class game:
         .replace("${auth_access_token}",auth_access_token).replace("${user_properties}",user_properties)\
         .replace("${user_type}",user_type)
         return parameter
+
+# 调试模式
+"""
+while True:
+    Input = input("MinecraftTechLauncher/sys64/py>")
+    if Input == 'help':
+        print("还没有搞好哦！")
+    elif Input == 'close':
+        quit()
+    elif Input == "build":
+        print(game().build())
+    elif Input[0:3] == "cmd":
+        os.system(Input[4:-1])
+    elif Input[0:2] == "py":
+        print(exec(Input[3:-1]))
+"""
+
 # GUI
+
+MainWind = tk.Tk()
+
+"""
+菜单 >>>
+一级菜单:<启动器> | <下载> | <高级> | <帮助>
+二级菜单:[启动器]:<设置> | <插件> | <检测错误>
+        :[下载]:<游戏本体> | <Mod> | <资源包> | <整合包> | <光影包>
+        :[高级]:<高级设置> | <插件> | <游戏参数> | <高级游戏设置>
+        :[帮助]:<帮助(软件内)> | <帮助(软件外)>
+"""
+
+# - 分割线 -
+
+MainWind.title('MinecraftTechLauncher')
+MainWind.geometry("600x450+374+182")
+MainWind.resizable(False,False)
+MainWind.mainloop()
