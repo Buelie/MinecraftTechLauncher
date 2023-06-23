dbf62489fdda725dded801f4d8854626170689ea
QingYunStudio <111875719+Buelie@users.noreply.github.com>
2023-06-23 15:24:54 +0800

    #2

--- main.py
+++ main.py
@@ -41,4 +41,17 @@ class JsonFile:
             f.close()
         return Data
 
+class game:
+    """
+    启动游戏类
+    build方法由于构建游戏启动参数并启动游戏
+    """
+    jdk = "C:\\Program Files\\Java\\jdk-17\\bin\\java.exe"
+
+    def __init__(self,cfg : str = "") -> None:
+        self.cfg = cfg
+
+    # TODO:待完成
+    def build(self,auth_player_name : str = "test",version_name : str = "",game_directory : str = "",assets_root : str = "",assets_index_name : str = "",auth_uuid : str = "12345678-012345678921-3456709312X456",auth_access_token : str = "",user_properties : str = "",user_type : str = "legacy"):
+        pass
 # GUI
