057fdcce14bc021e308ce76ba8bed7beb006ff4e
QingYunStudio <111875719+Buelie@users.noreply.github.com>
2023-06-23 15:13:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,26 @@ if os.path.isfile('config/cfg/main.json') == False:
         fe = json.dumps(Test)
         f.write(fe)
         f.close()
+# 函数
+class JsonFile:
+    """
+    读取/写入JSON文件的类
+    Read方法是读写JSON文件
+    Write方法是写入JSON文件
+    """
+    def __init__(self,path) -> None:
+        self.path = path
+
+    def Read(self):
+        with open(self.path,'w',encoding='utf-8') as f:
+            Data = json.load(f)
+            f.close()
+        return Data
+
+    def Write(self):
+        with open(self.path,'w',encoding='utf-8') as f:
+            Data = json.dumps(f)
+            f.close()
+        return Data
+
+# GUI
